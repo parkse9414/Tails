6458828fbac13b7074505ca62f0b394d168e9fa0
박세원 <parkse9414@naver.com>
2014-09-25 14:37:33 +0900

    jjjj

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+var trailX = [0,0,0,0]
+var trailY = [0,0,0,0]
+
 function setup()
 {
 	createCanvas(800,600)
@@ -8,5 +11,20 @@ function setup()
 function draw()
 {
 	background(0);
-	ellipse(400,300,mouseX,mouseY);
+
+	// mouse ellipse
+	ellipse(mouseX,mouseY,100,100)
+    
+    // trail ellipse #0
+	trailX[0] = (mouseX + trailX[0] * 9) / 10;
+	trailY[0] = (mouseY + trailY[0] * 9) / 10;
+    ellipse(trailX[0],trailY[0],80,80)
+
+    for (var i = 1; i < 4; i + 1)
+    {
+	trailX[i] = (trailX[i-1] + trailX[i] * 9) / 10;
+	trailY[i] = (trailY[i-1] + trailY[i] * 9) / 10;
+    ellipse(trailX[1],trailY[1],80,80)
+    }
+
 }
