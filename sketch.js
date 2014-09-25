fc8a628036951f4e3ad4d5677513a8ba4871aaca
박세원 <parkse9414@naver.com>
2014-09-25 15:01:03 +0900

    fgfgfg

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-var trailX = [0,0,0,0]
-var trailY = [0,0,0,0]
+var trailX = [0,0,0,0,0,0,0,0,0,0]
+var trailY = [0,0,0,0,0,0,0,0,0,0]
 
 function setup()
 {
-	createCanvas(800,600)
+	createCanvas(800,600);
 	noFill();
 	stroke(255,0,0);
 }
@@ -13,19 +13,22 @@ function draw()
 	background(0);
 
 	// mouse ellipse
-	ellipse(mouseX,mouseY,100,100)
+	ellipse(mouseX,mouseY,100,100);
     
     // trail ellipse #0
-	trailX[0] = (mouseX + trailX[0] * 9) / 10;
-	trailY[0] = (mouseY + trailY[0] * 9) / 10;
-    ellipse(trailX[0],trailY[0],80,80)
+	trailX[0] = (mouseX + trailX[0] * 5) / 6;
+	trailY[0] = (mouseY + trailY[0] * 5) / 6;
+    ellipse(trailX[0],trailY[0],80,80);
 
 
-    for (var i = 1; i < 4; i + 1)
+    for (var i = 1; i < 9; i=i + 1)
     {
-	trailX[i] = (trailX[i-1] + trailX[i] * 9) / 10;
-	trailY[i] = (trailY[i-1] + trailY[i] * 9) / 10;
-    ellipse(trailX[i],trailY[i],80,80)
+	trailX[i] = (trailX[i-1] + trailX[i] * 5) / 6;
+	trailY[i] = (trailY[i-1] + trailY[i] * 5) / 6;
+    ellipse(trailX[i],trailY[i],80,80);
     }
 
 }
+
+
+
